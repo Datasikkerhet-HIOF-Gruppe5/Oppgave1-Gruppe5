3e2bfb6c007f57dcd83b14bebd8cdc39d1c7ceb6
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2024-02-15 12:54:48 +0100

    Updated changePassword.php to work for professors

--- public_html/src/changePassword.php
+++ public_html/src/changePassword.php
@@ -1,38 +1,44 @@
 <?php
+session_start();
+
 include 'db_connect.php';
+$pdo = Database::getInstance();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    updatePassword($_POST, $_SESSION['user']['id']);
+    updatePassword($_POST, $pdo);
 }
 
-function updatePassword($data, $userId)
+function updatePassword($data, $pdo)
 {
-    $pdo = Database::getInstance();
-    $user = getUserById($userId, $pdo);
+    $professorId = $_SESSION['user_id'];
 
-    if (!$user || !password_verify($data['old_password'], $user['password'])) {
-        die("Incorrect old password");
+    $professor = getProfessorById($professorId, $pdo);
+
+    if (!$professor || !password_verify($data['old_password'], $professor['password'])) {
+        die("Incorrect old password.");
     }
 
     if ($data['new_password'] !== $data['confirm_password']) {
-        die("New passwords do not match");
+        die("New passwords do not match.");
     }
 
-    $query = "UPDATE users SET password = :password WHERE id = :id";
+    $query = "UPDATE professors SET password = :password WHERE id = :id";
+
     $stmt = $pdo->prepare($query);
 
     $newHashedPassword = password_hash($data['new_password'], PASSWORD_DEFAULT);
-    $stmt->bindParam(':password', $newHashedPassword);
-    $stmt->bindParam(':id', $userId);
 
+    $stmt->bindParam(':password', $newHashedPassword);
+    $stmt->bindParam(':id', $professorId);
     $stmt->execute();
 
-    die("Password updated successfully");
+    die("Password updated successfully. <a href='../index.html'>Go back to login</a>");
 }
 
-function getUserById($userId, $pdo)
+
+function getProfessorById($userId, $pdo)
 {
-    $query = "SELECT * FROM users WHERE id = :id";
+    $query = "SELECT * FROM professors WHERE id = :id";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':id', $userId);
     $stmt->execute();
