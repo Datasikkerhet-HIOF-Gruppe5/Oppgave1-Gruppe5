91561e1485f5b6c255063de5d3a1e3055d253dd1
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2024-04-11 12:57:38 +0200

    Oppdatert forgotpassword.php

--- public_html/src/forgotPassword.php
+++ public_html/src/forgotPassword.php
@@ -19,19 +19,28 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // Generate a unique token regardless of whether the email exists or not
         $token = bin2hex(random_bytes(32));
 
-        // Store the token and expiry time in the database
-        $expiryTime = time() + (60 * 60); // Token expires in 1 hour
-        $insertTokenQuery = "INSERT INTO password_reset (email, token, expiry_time) VALUES (:email, :token , :expiry_time)";
-        $stmt = $pdo->prepare($insertTokenQuery);
-        $stmt->execute([':email' => $email, ':token' => $token, ':expiry_time' => $expiryTime]);
+        // If the email exists, store the token and expiry time in the database
+        if ($user) {
+            $expiryTime = time() + (60 * 60); // Token expires in 1 hour
+            $insertTokenQuery = "INSERT INTO password_reset (email, token, expiry_time) VALUES (:email, :token , :expiry_time)";
+            $stmt = $pdo->prepare($insertTokenQuery);
+            $stmt->execute([':email' => $email, ':token' => $token, ':expiry_time' => $expiryTime]);
+        }
 
         // Send the reset email regardless of whether the email exists or not
         $resetLink = "http://158.39.188.207/steg1/public_html/src/passwordReset.php?token=$token";
         $subject = "Password Reset";
         $message = "Click the following link to reset your password: $resetLink";
 
-        // Send the email
-        mail($email, $subject, $message);
+        // If the email does not exist, do not actually send the email
+        if (!$user) {
+            // Log the fact that an email would have been sent here
+            // You can log this information to keep track of attempted password resets
+            // but ensure that you don't log the email address itself for privacy reasons.
+        } else {
+            // Send the email
+            mail($email, $subject, $message);
+        }
 
         // Provide a generic confirmation message
         echo "Password reset email sent, if the provided email is registered.";
